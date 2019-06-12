62de8ba64f4bdc8e22889c866e48dbf4deba9828
Roberto Sueiro Ventoso <rsv1002@alu.ubu.es>
2019-06-12 14:34:13 +0200

    Creeación de proyecto 2019PCTR_2C:
    3. Definición de constantes para el control de invariantes y definición de variables que se utilizarán en la clase Jugador.

--- src/pctr_2c_2019/Jugador.java
+++ src/pctr_2c_2019/Jugador.java
@@ -1,5 +1,7 @@
 package pctr_2c_2019;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  *
  * CLASE JUGADOR.
@@ -12,6 +14,36 @@ package pctr_2c_2019;
 
 public class Jugador {
 
+    // DEFINICIÓN DE CONSTANTES PARA CONTROL DE INVARIANTES
 
+    public static final int MIN_PELOTAS_NOVATOS = 2;			// Número
+
+    public static final int MAX_PELOTAS_NOVATOS = 5;			// Número
+
+    public static final int MAX_PALOS_NOVATOS = 2;				// Número
+
+    public static final int MAX_PELOTAS_EXPERTOS = 1;			// Número
+
+    public static final int MIN_PALOS_EXPERTOS = 2;				// Número
+
+    public static final int MAX_PALOS_EXPERTOS = 5;				// Número
+
+    // DEFINICIÓN DE VARIABLES
+
+    private int id;																	// Atributo de tipo int.
+
+    private boolean experto;														// Atributo de tipo boolean.
+
+    private int numVueltas;															// Atributo de tipo int.
+
+    private Club elClub;															// Objeto de tipo Club.
+
+    int tiempo = ThreadLocalRandom.current().nextInt(0, 999);				// Atributo de tipo int.
+
+    int pelotasActuales;															//
+
+    int palosActuales;																//
+
+    String ident;																	// Atributo de tipo string.
 
 }
