b1a91e69fd2dc97774e71e5555352b7fc77850d1
Roberto Sueiro Ventoso <rsv1002@alu.ubu.es>
2019-06-12 14:39:23 +0200

    Creeación de proyecto 2019PCTR_2C:
    4. Definición del constructor de la clase Jugador.

--- src/pctr_2c_2019/Jugador.java
+++ src/pctr_2c_2019/Jugador.java
@@ -16,17 +16,17 @@ public class Jugador {
 
     // DEFINICIÓN DE CONSTANTES PARA CONTROL DE INVARIANTES
 
-    public static final int MIN_PELOTAS_NOVATOS = 2;			// Número
+    public static final int MIN_PELOTAS_NOVATOS = 2;			// Constante para el control de pelotas mínimas para jugadores novatos.
 
-    public static final int MAX_PELOTAS_NOVATOS = 5;			// Número
+    public static final int MAX_PELOTAS_NOVATOS = 5;			// Constante para el control de pelotas máximas para jugadores novatos.
 
-    public static final int MAX_PALOS_NOVATOS = 2;				// Número
+    public static final int MAX_PALOS_NOVATOS = 2;				// Constante para el control de palos máximos para jugadores novatos.
 
-    public static final int MAX_PELOTAS_EXPERTOS = 1;			// Número
+    public static final int MAX_PELOTAS_EXPERTOS = 1;			// Constante para el control de pelotas máximas para jugadores expertos.
 
-    public static final int MIN_PALOS_EXPERTOS = 2;				// Número
+    public static final int MIN_PALOS_EXPERTOS = 2;				// Constante para el control de palos mínimos para jugadores expertos.
 
-    public static final int MAX_PALOS_EXPERTOS = 5;				// Número
+    public static final int MAX_PALOS_EXPERTOS = 5;				// Constante para el control de palos máximas para jugadores expertos.
 
     // DEFINICIÓN DE VARIABLES
 
@@ -38,12 +38,60 @@ public class Jugador {
 
     private Club elClub;															// Objeto de tipo Club.
 
-    int tiempo = ThreadLocalRandom.current().nextInt(0, 999);				// Atributo de tipo int.
+    int tiempo = ThreadLocalRandom.current().nextInt(0, 999);			// Atributo de tipo int.
 
-    int pelotasActuales;															//
+    int pelotasActuales;															// Atributo de tipo int.
 
-    int palosActuales;																//
+    int palosActuales;																// Atributo de tipo int.
 
     String ident;																	// Atributo de tipo string.
+
+    // DEFINICIÓN DE CONSTRUCTOR
+
+    /**
+     *
+     * DEFINICIÓN DE CONSTRUCTOR
+     *
+     * @param id - int
+     *
+     * @param experto - boolean
+     *
+     * @param numVueltas - int
+     *
+     * @param elClub - Club
+     *
+     */
+
+    public Jugador (int id, boolean experto, int numVueltas, Club elClub) {
+
+        ident = "";
+
+        ident += id;
+
+        this.id = id;
+
+        this.experto = experto;
+
+        this.numVueltas = numVueltas;
+
+        this.elClub = elClub;
+
+        this.palosActuales = 0;
+
+        this.pelotasActuales = 0;
+
+        // ASIGNACIÓN DE ATRIBUTO DE DIFERENCIACIÓN PARA NOVATO Y EXPERTO
+
+        if (experto) {
+
+            ident += "+";
+
+        } else {
+
+            ident += "-";
+
+        }
+
+    }
 
 }
