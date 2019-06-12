09e88254af2f05c6cb8c7ff603ea3805da9b3f2c
Roberto Sueiro Ventoso <rsv1002@alu.ubu.es>
2019-06-12 14:31:05 +0200

    Creeación de proyecto 2019PCTR_2C:
    2. Definición de las variables que se utilizarán en la clase Simulador.

--- src/pctr_2c_2019/Simulador.java
+++ src/pctr_2c_2019/Simulador.java
@@ -14,7 +14,21 @@ package pctr_2c_2019;
 
 public class Simulador {
 
-    public static void main(String[] args) {
+    // DEFINICIÓN DE VARIABLES
+
+    public static final int NUM_EXPERTOS = 7;													// Número máximo de jugadores expertos.
+
+    public static final int NUM_NOVATOS = 7;													// Número máximo de jugadores novatos.
+
+    public static final int NUM_PELOTAS = 20;													// Material disponible (máximo de pelotas).
+
+    public static final int NUM_PALOS = 20;														// Material disponible (máximo de palos).
+
+    public static final int NUM_VUELTAS = 5;													// Número de veces máximo que se repite el comportamiento.
+
+    // MÉTODO MAIN
+
+    public static void main (String[] args) {
 
 
 
