cc48f5bcde94f0dcca3e3f5f35d412d9c960323c
Roberto Sueiro Ventoso <rsv1002@alu.ubu.es>
2019-06-12 14:47:22 +0200

    Creeación de proyecto 2019PCTR_2C:
    8. Definición de variables en la clase Club.

--- src/pctr_2c_2019/Club.java
+++ src/pctr_2c_2019/Club.java
@@ -1,5 +1,7 @@
 package pctr_2c_2019;
 
+import java.util.ArrayList;
+
 /**
  *
  * CLASE CLUB.
@@ -14,6 +16,22 @@ package pctr_2c_2019;
 
 public class Club {
 
+    private boolean acceso;															//
 
+    // DEFINICIÓN DE ARRAYS PARA GESTIÓN DE JUGADORES
+
+    public static ArrayList<Jugador> jugadores = new ArrayList<Jugador>();			// Array de jugadores novatos.
+
+    // DEFINICIÓN DE MATERIAL DISPONIBLE.
+
+    private int pelotasActuales;												    // Número de pelotas iniciales disponibles.
+
+    private int palosActuales;													    // Número de palos iniciales disponibles.
+
+    //
+
+    private static final Object Lock = Club.class;									//
+
+    private static Club club;														//
 
 }
